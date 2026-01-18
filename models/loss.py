1979d30dc729369000dc405c29b7528e97df1dfe
D-Y5 <3268493835@qq.com>
2026-01-18 17:50:08 +0800

    修改API2

--- models/loss.py
+++ models/loss.py
@@ -236,7 +236,7 @@ def compute_iou(
             iou_values.append(iou_value)
         except:
             iou_values.append(0.0)
-    return paddle.to_tensor(iou_values, place=pred_corners.deplacevice)
+    return paddle.to_tensor(iou_values, place=pred_corners.place)
 
 
 def polygon_area(corners: paddle.Tensor) -> paddle.Tensor:
